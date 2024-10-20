88a7de7908bb143c0463baf24b2e3bcb5374d7fc
saba251103 <134624462+saba251103@users.noreply.github.com>
2024-10-20 13:01:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import Predict from './predict';
+import Newfile from './newfile';
+import Dashboard from './dashboard';
+import DisasterManagementTips from './tips';
 function App() {
   return (
     <Router>
       <div className='App' sx={{}}> 
       <Routes> 
         <Route path="/" element={<Home />} />
+        <Route path="/predict" element={<Predict />} />
+        <Route path="/newfile" element={<Newfile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/tips" element={<DisasterManagementTips />} />
+        
+
       </Routes>
       </div>
     </Router>
